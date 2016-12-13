05adb3be4fa783481472443620b6c528e8b51055
Matthew Dews <mdews233@gmail.com>
2016-12-13 04:45:50 -0500

    Add rate limit handling and more status getting functionality.

--- src/itsa/Runner.java
+++ src/itsa/Runner.java
@@ -3,33 +3,34 @@
  */
 package itsa;
 
-import java.util.Scanner;
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
-import java.io.FileInputStream;
-import java.io.InputStream;
-import java.net.UnknownHostException;
-import java.util.List;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.Date;
+import java.util.List;
 import java.util.Properties;
+import java.util.Scanner;
+import java.util.concurrent.TimeUnit;
+import java.util.logging.Logger;
+
+import org.bson.Document;
+
+import com.mongodb.MongoClient;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+import com.mongodb.client.model.Filters;
+import com.mongodb.client.model.Updates;
 
 import twitter4j.Paging;
 import twitter4j.Status;
 import twitter4j.Twitter;
 import twitter4j.TwitterException;
 import twitter4j.TwitterFactory;
-
-import com.mongodb.BasicDBObject;
-import com.mongodb.client.MongoCollection;
-import com.mongodb.client.MongoDatabase;
-import com.mongodb.DBCollection;
-import com.mongodb.DBCursor;
-import com.mongodb.MongoClient;
-import org.bson.Document;
 
 /**
  * @author Matthew Dews
@@ -56,6 +57,10 @@ public class Runner {
     shutdown();
   }
 
+  /*
+   * Initialization methods
+   */
+  
   private static void initialize() {
     System.out.println("Initializing...");
     initializeITSAProperties();
@@ -77,13 +82,13 @@ public class Runner {
       // Cleanup the input so we can parse it as a comma separated list
       u = u.replaceAll("\\s+","");
       usernames = Arrays.asList(u.split(","));
-      System.out.println("Loaded twitter usernames: " + usernames.toString());
+      //System.out.println("Loaded twitter usernames: " + usernames.toString());
 
       // Read start date for tweets
       SimpleDateFormat sdf = new SimpleDateFormat(DATE_FORMAT);
       try {
         START_DATE = sdf.parse(prop.getProperty("START_DATE"));
-        System.out.println("Loaded start date: " + START_DATE.toString());
+        //System.out.println("Loaded start date: " + START_DATE.toString());
       } catch (ParseException e) {
         System.err.println("Failed to parse the date from START_DATE=" + prop.getProperty("START_DATE"));
         e.printStackTrace();
@@ -91,7 +96,7 @@ public class Runner {
       }
       try {
         END_DATE = sdf.parse(prop.getProperty("END_DATE"));
-        System.out.println("Loaded end date: " + END_DATE.toString());
+        //System.out.println("Loaded end date: " + END_DATE.toString());
       } catch (ParseException e) {
         System.err.println("Failed to parse the date from END_DATE=" + prop.getProperty("END_DATE"));
         e.printStackTrace();
@@ -130,11 +135,14 @@ public class Runner {
   }
 
   private static void initializeMongo() {
-    //http://thinktostart.com/build-your-own-twitter-archive-and-analyzing-infrastructure-with-mongodb-java-and-r-part-1/
+    // Change log level so stdout is not filled with log messages
+    Logger mongoLogger = Logger.getLogger( "org.mongodb.driver" );
+    //mongoLogger.setLevel(Level.SEVERE); 
+    
     // Connect to DB
     System.out.println("Connecting to Mongo DB...");
-    MongoClient mongoClient = new MongoClient("127.0.0.1");
-    MongoDatabase database = mongoClient.getDatabase("tweets");
+    mongoClient = new MongoClient("127.0.0.1");
+    database = mongoClient.getDatabase("tweets");
     // System.out.println("Connected to Mongo DB successfully.");
 
     collection = database.getCollection("twitter");
@@ -147,29 +155,33 @@ public class Runner {
     while (!shouldExit) {
       System.out.println("Select a job: \n"
                        + "1. Collect twitter data\n"
+                       + "1a. Drop twitter data from Mongo\n"
                        + "2. Train SentiStrength\n"
                        + "3. Analyze twitter data with SentiStrength\n"
                        + "4. SAVE sentiment results to csv\n"
                        + "5. Exit\n");
       // TODO update jobs to include normalization
-      switch(reader.nextInt()) {
-      case 1:
-        System.out.println("WARNING: Function not fully implemented.");
+      switch(reader.nextLine()) {
+      case "1":
         collectAllStatuses();
         break;
-      case 2:
+      case "1a":
+        System.out.println("Dropping twitter data");
+        collection.drop();
+        break;
+      case "2":
         // train sentrength
         System.out.println("Function not yet implemented, sorry.");
         break;
-      case 3:
+      case "3":
         // analyze data
         System.out.println("Function not yet implemented, sorry.");
         break;
-      case 4:
+      case "4":
         // CSV generation
         System.out.println("Function not yet implemented, sorry.");
         break;
-      case 5:
+      case "5":
         // exit
         reader.close();
         System.out.println("EXITING");
@@ -182,54 +194,68 @@ public class Runner {
     }
   }
 
+  /*
+   * Status collection methods
+   */
+  
   private static void collectAllStatuses() {
     for (String u : usernames) {
-      collectStatusesInRange(END_DATE, START_DATE, u);
-      Status latestStatus = getFirstStatusBefore(END_DATE, u);
-      System.out.println("Latest Tweet from " + latestStatus.getUser().getName() + ":\n"
-          + latestStatus.getText() + "\n"
-          + "Before date " + END_DATE);
+      System.out.println("Collecting Statuses for user: " + u);
+      collectStatusesForUser(u);
     }
   }
 
-  private static List<Status> collectStatusesInRange(Date latest, Date earliest, String username) {
+  private static void collectStatusesForUser(String username) {
     Document doc = new Document()
         .append("username", username)
-        .append("numTweets", 0);
-    collection.insertOne(doc);
+        .append("tweets", new ArrayList<>());
+    int tweetsRecordedInMongo = 0;
+    collection.insertOne(doc); // Insert starter doc for user
     Status status = getFirstStatusBefore(END_DATE, username);
     long maxId = status.getId() + 1; // Add one so the first status is included in paging.
-    ArrayList<Status> completeStatuses = new ArrayList<Status>();
+    
     try {
       int page = 1;
       List<Status> pagedStatuses = Collections.emptyList();
+      Boolean reachedEnd = false;
       do {
+        handleUserTimeLineRateLimit();
         pagedStatuses = twitter.getUserTimeline(username, new Paging(page, PAGE_SIZE, 1, maxId - 1));
         for (Status s : pagedStatuses) {
-          status = s;
-          if ((status.getCreatedAt()).before(earliest)) {
-            //return status;
+          if ((s.getCreatedAt()).before(START_DATE)) { // Stop if past the START date
+            reachedEnd = true;
+            break;
           }
+          // Add status to Document
+          Document tweet = new Document()
+                                .append("text", s.getText())
+                                .append("date",s.getCreatedAt().toString());
+
+          collection.updateOne(Filters.eq("username", username),Updates.addToSet("tweets", tweet));
+          System.out.println(++tweetsRecordedInMongo + " tweets recorded to mongo so far");
         }
-      } while (false);
+        
+      } while (!reachedEnd);
 
     } catch (TwitterException e) {
-
+      System.err.println("Error in collectStatusesForUser:  " + username +":\n"
+          + e.getErrorMessage());
     }
-    return null;
   }
 
   /**
    * Find the first status posted before @date by @username
+   * Does not handle edge cases properly :)
    * @param date
    * @param username
-   * @return id
+   * @return Status
    */
   private static Status getFirstStatusBefore(Date date, String username) {
     Status status = null;
 
     try {
       int page = 1; //Initial page
+      handleUserTimeLineRateLimit();
       status = twitter.getUserTimeline(username, new Paging(1,1)).get(0); // Latest tweet
       // Edge case that the first tweet matches.
       if ((status.getCreatedAt()).before(date)) {
@@ -237,6 +263,7 @@ public class Runner {
       }
 
       do {
+        handleUserTimeLineRateLimit();
         List<Status> statuses = twitter.getUserTimeline(username, new Paging(page, PAGE_SIZE, 1, status.getId() - 1));
         // iterate over statuses
         for (Status s : statuses) {
@@ -257,7 +284,34 @@ public class Runner {
     return status;
   }
   
-  // Will return null if there are no tweets after a certain date :/ 
+  private static void handleUserTimeLineRateLimit() {
+  //System.out.println("Remaining: " + twitter.getUserTimeline().getRateLimitStatus().getRemaining());
+    try {
+      if(twitter.getUserTimeline().getRateLimitStatus().getRemaining() <= 3){
+        try {
+          System.out.println("Stopping for " + twitter.getUserTimeline().getRateLimitStatus().getSecondsUntilReset()
+                            + " seconds for rate limiting");
+          TimeUnit.SECONDS.sleep(twitter.getUserTimeline().getRateLimitStatus().getSecondsUntilReset() + 2);
+          System.out.println("Resuming operation after rate limit sleep");
+        } catch (InterruptedException e) {
+          System.err.println("Something went wrong while sleeping");
+          e.printStackTrace();
+        }
+      }
+    } catch (TwitterException e) {
+      System.err.println("Twitter error during Rate Limit check");
+      e.printStackTrace();
+    }
+  }
+
+  /**
+   * Find the first status posted after @date by @username
+   * Does not handle edge cases properly :)
+   * Will return null if there are no tweets after a given date
+   * @param date
+   * @param username
+   * @return Status
+   */
   private static Status getFirstStatusAfter(Date date, String username) {
     Status status = null;
     Status nextStatus = null;
@@ -266,13 +320,12 @@ public class Runner {
       int page = 1; //Initial page
       nextStatus = twitter.getUserTimeline(username, new Paging(1,1)).get(0); // Latest tweet
       // Edge case that the first tweet matches.
-      // TODO fix this
       if ((nextStatus.getCreatedAt()).before(date)) {
         return null;
       }
 
       do {
-        List<Status> statuses = twitter.getUserTimeline(username, new Paging(page, PAGE_SIZE, 1, status.getId() - 1));
+        List<Status> statuses = twitter.getUserTimeline(username, new Paging(page, PAGE_SIZE, 1, nextStatus.getId() - 1));
         // iterate over statuses
         for (Status s : statuses) {
           status = nextStatus;
@@ -292,6 +345,10 @@ public class Runner {
     
     return status;
   }
+  
+  /*
+   * Shutdown methods
+   */
   
   private static void shutdown() {
     shutdownMongo();
