bb7b3bfbf4d086d759036870bc050f92436c522d
Matthew Dews <matthew-dews@users.noreply.github.com>
2016-12-27 18:18:17 -0500

    Remove broken code from Util.removeURL

--- src/itsa/Util.java
+++ src/itsa/Util.java
@@ -2,8 +2,6 @@ package itsa;
 
 import java.io.File;
 import java.util.Scanner;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 public class Util {
     
@@ -14,17 +12,7 @@ public class Util {
         return  file.exists();
     }
     
-    // https://stackoverflow.com/questions/12366496/removing-the-url-from-text-using-java
     public static String removeUrl(String string) {
-//        String urlPattern = "((https?|ftp|gopher|telnet|file|Unsure|http):((//)|(\\\\))+[\\w\\d:#@%/;$()~_?\\+-=\\\\\\.&]*)";
-//        Pattern p = Pattern.compile(urlPattern,Pattern.CASE_INSENSITIVE);
-//        Matcher m = p.matcher(commentstr);
-//        int i = 0;
-//        while (m.find()) {
-//            commentstr = commentstr.replaceAll(m.group(i),"").trim();
-//            i++;
-//        }
-//        return commentstr;
         return string.replaceAll("https?://\\S+\\s?", "");
     }
     
